7ddefe2f406e4eac6d470f07dab3919af4d197cb
adiyenuganti501 <yenuganti501@gmail.com>
2025-07-16 12:50:49 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
-import './App.css';
+//import './App.css';
+import React ,{useState} from 'react';
+import {BrowserRouter as Router ,Route, Routes } from "react-router-dom" ;
+import MainTodo from './MainTodo';
+// import FoodOrder from './FoodOrder';
+//import NavBar from './NavBar'
+// import Gallery from './Gallery';
+// import Movies from './Movies';
+// import FormValidation from './FormValidation'
+//import Footer from './Footer';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MainTodo/>
     </div>
   );
 }
