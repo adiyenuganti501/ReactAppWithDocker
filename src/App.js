00715bdedce1525b5e0e7a90e1423c374fcf3dc4
adiyenuganti501 <54026439+adiyenuganti501@users.noreply.github.com>
2026-06-15 08:26:57 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import logo from './logo.svg';
-//  import './App.css';
+// // import './App.css';
 import React ,{useState} from 'react';
 //import {BrowserRouter as Router ,Route, Routes } from "react-router-dom" ;
 import MainTodo from './MainTodo';
